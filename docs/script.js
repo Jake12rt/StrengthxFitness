55a9665bd4dc64deee9079e72734b5e411a0b722
Jake Portacio <12rt.my@gmail.com>
2024-12-01 10:47:00 +0800

    Add files via upload

---
+++ docs/script.js
@@ -0,0 +1,20 @@
+
+// Close the modal when the user clicks outside of it
+window.onclick = function(event) {
+   const modals = document.getElementsByClassName('modal');
+   Array.from(modals).forEach(function(modal) {
+       if (event.target == modal) {
+           modal.style.display = 'none';
+       }
+   });
+}
+
+
+function toggleMobileMenu() {
+   const navLinksContainer = document.querySelector('.nav-links-container');
+   const menubar = document.querySelector('.menubar');
+
+   navLinksContainer.classList.toggle('active');
+   menubar.classList.toggle('active');
+}
+
